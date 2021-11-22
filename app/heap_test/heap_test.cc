b4f8da70ad04562c6f9b86a8500c15b0043392a3
eduardolussi <eduardolussi@gmail.com>
2021-11-22 13:04:19 -0300

    Template testes

--- app/heap_test/heap_test.cc
+++ app/heap_test/heap_test.cc
@@ -4,9 +4,23 @@ using namespace EPOS;
 
 OStream cout;
 
+void stress_test()
+{
+
+}
+
+void method_test()
+{
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Running stress test" << endl;
+    stress_test();
+
+    cout << "Running First-Fit correctness test" << endl;
+    method_test();
 
     return 0;
 }
