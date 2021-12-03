196f1813755a88aaa1e1491a39104edece9f9d20
Eduardo Lussi <eduardolussi@localhost.localdomain>
2021-12-03 13:19:27 -0300

    Test missing busy waiting

--- app/RM_test/RM_test.cc
+++ app/RM_test/RM_test.cc
@@ -13,27 +13,27 @@ RT_Thread* tasks[3];
 
 OStream cout;
 
-void task0() {
+void A() {
     cout << "Thread A activated" << endl;
-    for (int i = 0; i < 99999; i++);
+    // Busy wait 200000
     cout << "Thread A finished" << endl;
 }
 
-void task1() {
+void B() {
     cout << "Thread B activated" << endl;
-    for (int i = 0; i < 99999; i++);
+    // Busy wait 400000
     cout << "Thread B finished" << endl;
 }
 
-void task2() {
+void C() {
     cout << "Thread C activated" << endl;
-    for (int i = 0; i < 99999; i++);
+    // Busy wait 1000000
     cout << "Thread C finished" << endl;
 }
 
 int main()
 {
-    tasks[0] = new RT_Thread(&task0, 1000000, 1000000);
-    tasks[1] = new RT_Thread(&task1, 1500000, 1500000);
-    tasks[2] = new RT_Thread(&task2, 3500000, 3500000);
+    tasks[0] = new RT_Thread(&A, 1000000, 1000000);
+    tasks[1] = new RT_Thread(&B, 1500000, 1500000);
+    tasks[2] = new RT_Thread(&C, 3500000, 3500000);
 }
